9d228e535c22e734a697a94cf908861d1fe80eba
ksl24 <ksl24@ensc-mmc-06.engineering.sfu.ca>
2018-10-16 20:18:01 -0700

    Finished multithreaded implementation of mapper calls

--- mapreduce_host.h
+++ mapreduce_host.h
@@ -12,15 +12,16 @@
 #include <utility>
 #include <algorithm>
 #include <vector>
-#define NUM_CORES 4 //4 for the laptop
+#define NUM_CORES 12 //4 for the laptop, 12 for pit machines
 
 
-struct kv_pair{
-    std::string key;
-    int value;
+template <typename keytype, typename valuetype>
+struct threaded_map_func_input{
+    int thread_num;
+    std::pair<keytype, valuetype> (*map_func) (keytype);
+    std::vector<keytype> parsed_input;
+    std::vector<std::pair<keytype, valuetype> >* ret_p;
 };
-
-
 
 template<class keytype, class valuetype>
 bool mapvect_sort_func(std::pair<keytype, valuetype> a, std::pair<keytype, valuetype> b){
@@ -28,12 +29,21 @@ bool mapvect_sort_func(std::pair<keytype, valuetype> a, std::pair<keytype, value
 }
 
 template<class keytype, class valuetype>
-std::vector<std::pair<keytype, valuetype> > threaded_map_func(std::vector<keytype> parsed_input, int thread_num);
+void* threaded_map_func(void* vp_input){
     using namespace std;
 
-    for (int i = thread_num; i < parsed_input.size(); i += NUM_THREADS){
-        
-        
+    // Grab the input values from the void pointer:
+    struct threaded_map_func_input<keytype, valuetype>* input = static_cast<struct threaded_map_func_input<keytype, valuetype>* >(vp_input);
+    int thread_num = input->thread_num;
+    pair<keytype, valuetype> (*map_func) (keytype) = input->map_func;
+    vector<keytype> parsed_input = input->parsed_input;
+    vector<pair<keytype, valuetype> >* ret_p = input->ret_p;
+
+    for (int i = thread_num; i < parsed_input.size(); i += NUM_CORES){
+        pair<keytype, valuetype> to_push = map_func(parsed_input[i]);
+        (*ret_p).push_back(to_push);
+    }
+}   
 
 
 
@@ -48,11 +58,31 @@ void mapreduce(std::vector<keytype> input_reader (void*), std::pair<keytype, val
 
     /* MAPPER: */
     vector<pair<keytype, valuetype> > mapped_kv_pairs;
+    void* ret;
     pthread_t threads[NUM_CORES];
+    vector<pair<keytype, valuetype> > threaded_mapfunc_retvals[NUM_CORES];
 
     for (int i = 0; i < NUM_CORES; ++i){
-        mapped_kv_pairs.push_back(map_func(parsed_input[i]));
-    }   
+        // Create the input to the threaded_map_func:
+        threaded_map_func_input<keytype, valuetype>* input = new threaded_map_func_input<keytype, valuetype>;
+        (*input).thread_num = i;
+        (*input).map_func = map_func;
+        (*input).parsed_input = parsed_input;
+        (*input).ret_p = &threaded_mapfunc_retvals[i];
+
+        // And create a thread to run the function.
+        pthread_create(&threads[i], NULL, threaded_map_func<keytype, valuetype>, static_cast<void*>(input));
+    }
+
+    for (int i = 0; i < NUM_CORES; ++i){
+        // Join the threads, so we can get their values
+        pthread_join(threads[i], NULL);
+        vector<pair<keytype, valuetype> > mapped_kv_subVect_p = threaded_mapfunc_retvals[i];
+        for (int j = 0; j < mapped_kv_subVect_p.size(); ++j){
+            pair<keytype, valuetype> cur = mapped_kv_subVect_p[i];
+            mapped_kv_pairs.push_back(cur);
+        }
+    }
     /* END OF MAPPER SECTION */
 
 
